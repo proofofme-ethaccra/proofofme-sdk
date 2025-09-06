eb68861a24aee6eefbbdd1e25867ceb7083ddb49
ishola-faazele <faazeleishola2030@gmail.com>
2025-09-06 08:21:43 +0000

    minor changes

--- packages/apis/infra/abi.ts
+++ packages/apis/infra/abi.ts
@@ -1,8 +1,4 @@
-import { CredentialRegistryAbi, ProofOfMeAbi } from "@proofofme/contracts";
+import { ProofOfMeAbi } from "@proofofme/contracts";
 export function getProofMeABI(): any[] {
   return ProofOfMeAbi.abi;
-}
-
-export function getCredentialRegistry(): any[] {
-  return CredentialRegistryAbi.abi;
 }
--- packages/contracts/index.ts
+++ packages/contracts/index.ts
@@ -1,11 +1,4 @@
-// Import the ABI
 import ProofOfMeContract from "./artifacts/contracts/ProofOfMe.sol/ProofOfMe.json";
-import CredentialRegistry from "./artifacts/contracts/CredentialRegistry.sol/CredentialRegistry.json";
-// Export the ABI
+
+// Export only named exports
 export const ProofOfMeAbi = ProofOfMeContract;
-export const CredentialRegistryAbi = CredentialRegistry;
-// Default export if needed
-export default {
-  ProofOfMeAbi,
-  CredentialRegistryAbi,
-};
