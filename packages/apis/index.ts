4d992d5b6bec7e0eeddf51cb7cd60661fabc9ba3
ishola-faazele <faazeleishola2030@gmail.com>
2025-09-06 08:16:28 +0000

    minor changes

--- packages/apis/index.ts
+++ packages/apis/index.ts
@@ -5,13 +5,9 @@ export * from "./domain/DIDDocument";
 export * from "./domain/types";
 
 // Export services
-export * from "./services/CredentialTypeService";
-export * from "./services/IssueClaimService";
-export * from "./services/RegistrationService";
-export * from "./services/VerifyClaimService";
+export * from "./controller/services";
 
 // Export infrastructure services
 export * from "./infra/EncryptionService";
 export * from "./infra/EthereumService";
-export * from "./infra/FilecoinService";
 export * from "./infra/KeyService";
